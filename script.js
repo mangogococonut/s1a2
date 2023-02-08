549524836e2c1b76f8bb0d2a6b7da67afda0c147
mangogococonut <124616597+mangogococonut@users.noreply.github.com>
2023-02-08 12:10:19 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+class CreatePattern {
+    constructor() {
+        this.color = presetColor.palette[mFmR(presetColor.palette.length)]
+        this.initialStyles = document.createElement('i')
+    }
+
+    //添加class
+    growUp(pattern, x, y) {
+        this.initialStyles.classList.add(pattern)
+        this.initialStyles.style.cssText = `background-color:${this.color};left:${x}px;top:${y}px;z-index:999`
+        abc.append(this.initialStyles)
+    }
+
+    //随机散开
+    dynamic() {
+        let sc = 1, x = 0, y = 0
+
+        function trans(a, b, c) {
+            if (b) {
+                a -= c
+            } else {
+                a += c
+            }
+            return a
+        }
+        //骰子
+        let dice = function (a, b) {
+            return a < 6 && b < 6 ? dice(mFmR(20), mFmR(20)) : [a, b]
+        }
+        let dd = dice(mFmR(20), mFmR(20));
+
+        let polePositiveNegative = [mFmR(2), mFmR(2)]
+
+        let animating = setInterval(() => {
+            //sc归零动画结束
+            if (sc <= 0) {
+                clearInterval(animating)
+                this.initialStyles.remove()
+            } else {
+                sc -= 0.1
+                x = trans(x, polePositiveNegative[0], dd[0]);
+                y = trans(y, polePositiveNegative[1], dd[1]);
+                this.initialStyles.style.transform = `translate(${x}px,${y}px) scale(0)`
+            }
+        }, 99);
+    }
+}
+
+function mFmR(value) {
+    return Math.floor(Math.random() * value)
+}
+
+//调色盘
+const presetColor = { palette: ["#eae395", "#495466", "#7d9ba3", "#a7b0ad","#ea9e7d"] }
+
+function produceStyle(pattern, x, y) {
+    let produce = new CreatePattern()
+    produce.growUp(pattern, x, y)
+    produce.dynamic()
+}
+
+let abc = document.querySelector('.abc')
+
+document.addEventListener('click', function (e) {
+    for (let i = 0; i < 10; i++) {
+        produceStyle('aixin', e.pageX, e.pageY)
+    }
+})
